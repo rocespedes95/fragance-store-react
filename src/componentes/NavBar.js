29c3639cfdbb1d53ff9c0649c0a2fdf7b5bf862d
rocespedes <rodrigo_cespedes@hotmail.com.ar>
2022-12-18 22:38:59 -0300

    subida al git

---
+++ src/componentes/NavBar.js
@@ -0,0 +1,16 @@
+
+export const NavBar =() => {
+    return(
+        <header className="header">
+            <h2>Fragance Store</h2>
+            <nav className="nav">
+                <a className="link_nav" href="#">Men</a>
+                <a className="link_nav" href="#">Women</a>
+                <a className="link_nav" href="#">Kid</a>
+                <a className="link_nav" href="#">Ubicacion</a>
+                <a className="link_nav" href="#">Contacto</a>
+            </nav>
+        </header>
+       
+    )
+}
